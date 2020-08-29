bc2ff9e568ac3c54d5f7fe48c069db51c48be465
Jp1200 <jprkr96@gmail.com>
2020-08-29 11:31:51 -0500

    Solution 8

--- ProductSum.py
+++ ProductSum.py
@@ -1,3 +1,10 @@
 def productSum(array):
-    ans = 0
-    return ans
+ ans = 0
+	for item in array:
+		if type(item) is list:
+			ans += productSum(item, depth+1)
+		else:
+			ans += item
+    return ans * depth
+
+
